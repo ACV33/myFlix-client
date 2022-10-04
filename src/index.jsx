c4b5dac78df3b740e3408c016462034c3331e8b1
ACV33 <ashli.vaccaro@gmail.com>
2022-10-04 19:27:41 -0400

    add new movie-card and movie-view files

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainView from 'src/components/main-view/main-view';
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,11 +8,11 @@ import './index.scss';
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
-        return (
-            <MainView />
-        );
+        return
+        <MainView />;
     }
 }
+
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
