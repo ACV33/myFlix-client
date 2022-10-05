4f004905114a92c0cdbd6b55e7038f3525872e24
ACV33 <ashli.vaccaro@gmail.com>
2022-10-05 19:00:52 -0400

    updated files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            movies: [
-                {
-                    _id: 1,
-                    Title: 'Inception',
-                    Description:
-                        'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-                    ImagePath: posterInception,
-                },
-                {
-                    _id: 2,
-                    Title: 'The Shawshank Redemption',
-                    Description:
-                        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-                    ImagePath: posterShawshank,
-                },
-                {
-                    _id: 3,
-                    Title: 'Gladiator',
-                    Description:
-                        'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
-                    ImagePath: posterGladiator,
-                },
-            ],
+            movies: [],
             selectedMovie: null,
         };
     }
 
+    componentDidMount() {
+        axios.get('https://ashlis-movie-api.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     setSelectedMovie(newSelectedMovie) {
         this.setState({
             selectedMovie: newSelectedMovie,
@@ -41,30 +31,29 @@ class MainView extends React.Component {
     }
 
     render() {
-        const { movies } = this.state;
+        const { movies, selectedMovie } = this.state;
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
-                {selectedMovie ? (
+                {selectedMovie ?
                     <MovieView
                         movie={selectedMovie}
                         onBackClick={(newSelectedMovie) => {
                             this.setSelectedMovie(newSelectedMovie);
-                        }}
-                    />
-                ) : (
-                    movies.map((movie) => (
+                        }} />
+
+                    : movies.map((movie) => (
                         <MovieCard
                             key={movie._id}
                             movie={movie}
-                            onMovieClick={(movie) => {
-                                this.setSelectedMovie(movie);
+                            onMovieClick={(newSelectedMovie) => {
+                                this.setSelectedMovie(newSelectedMovie);
                             }}
                         />
                     ))
-                )}
+                }
             </div>
         );
     }
